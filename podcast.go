4bca9017cd691d7eb42a1d34173635fd8cd15a91
vidhur2k <vidhur2k@gmail.com>
2021-02-20 14:48:42 -0500

    Updated RSS feed header to include content

--- podcast.go
+++ podcast.go
@@ -15,7 +15,7 @@ import (
 // Constants to use while generating podcast feed.
 const (
 	pVersion = "1.3.1"
-	HEADER   = `<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"   xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"  xmlns:content="http://purl.org/rss/1.0/modules/content/">`
+	HEADER   = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<rss version=\"2.0\" xmlns:itunes=\"http://www.itunes.com/dtds/podcast-1.0.dtd\" xmlns:content=\"http://purl.org/rss/1.0/modules/content/\">\n"
 	ITUNESNS = "http://www.itunes.com/dtds/podcast-1.0.dtd"
 )
 
