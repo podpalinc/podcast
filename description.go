29e245d7743808543a1f1cefc01695100d60a50c
vidhur2k <vidhur2k@gmail.com>
2021-05-23 14:51:08 -0400

    Added cdata for description at the item and channel level

--- description.go
+++ description.go
@@ -2,6 +2,9 @@ package podcast
 
 import "encoding/xml"
 
+/**
+* This description will be used at both the channel and item level to provide the ability to add hyperlinks, formatted text, etc.
+ */
 type Description struct {
 	XMLName xml.Name `xml:"description"`
 	Text    string   `xml:",cdata"`
